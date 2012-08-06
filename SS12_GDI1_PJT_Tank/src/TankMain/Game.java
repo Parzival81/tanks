aedccd35a965e5e0202f165d7ad9736621a4f0d0
Patrick Dorrington <patrick@dorrington.de>
2012-08-06 15:21:10 +0200

    Uncommented code in Game.java
    - Added comments for better code comprehension
    - Uncommented map load so one can build

--- SS12_GDI1_PJT_Tank/src/TankMain/Game.java
+++ SS12_GDI1_PJT_Tank/src/TankMain/Game.java
@@ -1,11 +1,14 @@
 package TankMain;
 
+import Elements.*;
+import eea.engine.action.basicactions.*;
+import eea.engine.component.render.*;
+import eea.engine.entity.*;
+import eea.engine.event.basicevents.*;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.LinkedList;
-
-import org.newdawn.slick.Color;
 import org.newdawn.slick.GameContainer;
 import org.newdawn.slick.Graphics;
 import org.newdawn.slick.Image;
@@ -15,181 +18,156 @@ import org.newdawn.slick.geom.Vector2f;
 import org.newdawn.slick.state.BasicGameState;
 import org.newdawn.slick.state.StateBasedGame;
 
-import Elements.*;
+public class Game extends BasicGameState {
+
+    private int stateID;
+    private StateBasedEntityManager entityManager;
+    private Level gamelevel;
+
+    public Game(int id) {
+        stateID = id;
+        entityManager = StateBasedEntityManager.getInstance();
+    }
+
+    @Override
+    public void init(GameContainer arg0, StateBasedGame arg1)
+            throws SlickException {
+        // EINLESEN VON DATEI
+        // Hier wird eine Karte eingelesen und alle Attribute die fuer die Darstellung der
+        // Karte gebraucht werden, werden hier gesetzt.
+        dataReader();
+
+        // DARSTELLUNG (DISPLAY)
+        // Hier werden alle Attribute auf dem Bildschirm dargestellt
+
+        //Hintergrund
+        Entity background = new Entity("menu");
+        background.setPosition(new Vector2f(400, 300));
+        background.addComponent(new ImageRenderComponent(new Image(gamelevel.getGameMap().getBackground())));
+        entityManager.addEntity(stateID, background);
+        //Escape Taste
+        Entity esc_Listener = new Entity("ESC_Listener");
+        KeyPressedEvent esc_pressed = new KeyPressedEvent(Input.KEY_ESCAPE);
+        esc_pressed.addAction(new ChangeStateAction(Launch.MENU));
+        esc_Listener.addComponent(esc_pressed);
+        entityManager.addEntity(stateID, esc_Listener);
+
+    }
+
+    public void dataReader() {
+        try {
+            BufferedReader br = new BufferedReader(new FileReader("maps/map00"));
+            String line;
+
+            LinkedList<Border> borderlist = new LinkedList<Border>();
+            LinkedList<Tank> tanklist = new LinkedList<Tank>();
+            LinkedList<Wall> walllist = new LinkedList<Wall>();
+            //Attribute die gesetzt werden muessen
+            Map m = null;
+
+            while ((line = br.readLine()) != null) {
+                if (line.contains("Map")) {
+                    LinkedList<String> ll = new LinkedList<String>();
+                    for (int i = 0; i < line.length(); i++) {
+                        StringBuffer sb = new StringBuffer();
+                        while (i < line.length() && line.charAt(i) != ' ') {
+                            sb.append(line.charAt(i));
+                            i++;
+                        }
+                        ll.add(sb.toString().replaceAll("\"", ""));
+                    }
+                    System.out.println(ll.toString());
+                    m = new Map(ll.get(1), ll.get(2), ll.get(3),
+                            Integer.valueOf(ll.get(4)), Integer.valueOf(ll.get(5)), Integer.valueOf(ll.get(6)));
 
 
-import eea.engine.action.Action;
-import eea.engine.action.basicactions.*;
-import eea.engine.component.render.*;
-import eea.engine.entity.*;
-import eea.engine.event.*;
-import eea.engine.event.basicevents.*;
+                } else if (line.contains("Border")) {
+                    LinkedList<String> ll = new LinkedList<String>();
+                    for (int i = 0; i < line.length(); i++) {
+                        StringBuffer sb = new StringBuffer();
+                        while (i < line.length() && line.charAt(i) != ' ') {
+                            sb.append(line.charAt(i));
+                            i++;
+                        }
+                        ll.add(sb.toString());
+                    }
 
-public class Game extends BasicGameState{
-	
-	private int stateID; 						
-	private StateBasedEntityManager entityManager;
-	private Level gamelevel;
-	
-	public Game(int id){
-		stateID = id;
-		entityManager = StateBasedEntityManager.getInstance();
-	}
+                    Border bo = new Border(Integer.valueOf(ll.get(1)), Integer.valueOf(ll.get(2)),
+                            Integer.valueOf(ll.get(3)), Integer.valueOf(ll.get(4)));
+                    borderlist.add(bo);
 
-	@Override
-	public void init(GameContainer arg0, StateBasedGame arg1)
-			throws SlickException {
-		// EINLESEN VON DATEI
-		// Hier wird eine Karte eingelesen und alle Attribute die fuer die Darstellung der
-		// Karte gebraucht werden, werden hier gesetzt.
-		dataReader();
-		
-		
-		// DARSTELLUNG (DISPLAY)
-		// Hier werden alle Attribute auf dem Bildschirm dargestellt
-		
-		
-		
-		//Hintergrund
-    	Entity background = new Entity("menu");	
-    	background.setPosition(new Vector2f(400,300));
-    	background.addComponent(new ImageRenderComponent(new Image(gamelevel.getGameMap().getBackground())));    	    	
-    	entityManager.addEntity(stateID, background);
-    	//Escape Taste
-    	Entity esc_Listener = new Entity("ESC_Listener");
-    	KeyPressedEvent esc_pressed = new KeyPressedEvent(Input.KEY_ESCAPE);
-    	esc_pressed.addAction(new ChangeStateAction(Launch.MENU));
-    	esc_Listener.addComponent(esc_pressed);    	
-    	entityManager.addEntity(stateID, esc_Listener);
-    	
-    	
-    	
-    	
-		
-	}
-	
-	public void dataReader(){
-		try {
-    		BufferedReader br = new BufferedReader(new FileReader("maps/map00"));
-    		String line;
-    		
-    		
-    		LinkedList <Border> borderlist = new LinkedList<Border>();
-    		LinkedList <Tank> tanklist = new LinkedList<Tank>();
-    		LinkedList <Wall> walllist = new LinkedList<Wall>();
-    		//Attribute die gesetzt werden muessen
-    		Map m = null;
-    		
-    		
-    		while ((line = br.readLine()) != null) {
-    			if (line.contains("Map")){
-    				LinkedList <String> ll = new LinkedList<String>();
-    				for (int i=0; i < line.length();i++){
-    						StringBuffer sb = new StringBuffer();
-    						while (i < line.length() && line.charAt(i) != ' '){
-    							sb.append(line.charAt(i));
-    							i++;
-    						}
-    						ll.add(sb.toString().replaceAll("\"", ""));    					
-    				}
-    				System.out.println(ll.toString());
-    				m = new Map (ll.get(1), ll.get(2), ll.get(3), 
-    						Integer.valueOf(ll.get(4)), Integer.valueOf(ll.get(5)), Integer.valueOf(ll.get(6)));
-    				
-    				
-    			}else if (line.contains("Border")){
-    				LinkedList <String> ll = new LinkedList<String>();
-    				for (int i=0; i < line.length();i++){
-    						StringBuffer sb = new StringBuffer();
-    						while (i < line.length() && line.charAt(i) != ' '){
-    							sb.append(line.charAt(i));
-    							i++;
-    						}
-    						ll.add(sb.toString());    					
-    				}
-    				
-    				Border bo = new Border(Integer.valueOf(ll.get(1)),Integer.valueOf(ll.get(2)),
-    						Integer.valueOf(ll.get(3)),Integer.valueOf(ll.get(4)));
-    				borderlist.add(bo);
-    				
-  
-    			}else if (line.contains("Tank")){
-    				LinkedList <String> ll = new LinkedList<String>();
-    				for (int i=0; i < line.length();i++){
-    						StringBuffer sb = new StringBuffer();
-    						while (i < line.length() && line.charAt(i) != ' '){
-    							sb.append(line.charAt(i));
-    							i++;
-    						}
-    						ll.add(sb.toString().replaceAll("\"", ""));    					
-    				}
-    				Tank ta = new Tank(ll.get(1), Integer.valueOf(2), Integer.valueOf(3)
-    						, Integer.valueOf(4), Integer.valueOf(5), Integer.valueOf(6)
-    						, Integer.valueOf(7), Integer.valueOf(8), Integer.valueOf(9)
-    						, Integer.valueOf(10), Integer.valueOf(11), Integer.valueOf(12)
-    						, Integer.valueOf(13));
-    				
-    				tanklist.add(ta);
-    			}else if (line.contains("Wall")){
-    				LinkedList <String> ll = new LinkedList<String>();
-    				for (int i=0; i < line.length();i++){
-    						StringBuffer sb = new StringBuffer();
-    						while (i < line.length() && line.charAt(i) != ' '){
-    							sb.append(line.charAt(i));
-    							i++;
-    						}
-    						ll.add(sb.toString());    					
-    				}
-    				Wall wa = new Wall(Integer.valueOf(1),Integer.valueOf(2),Integer.valueOf(3),
-    						Integer.valueOf(4),Integer.valueOf(5),Integer.valueOf(6));
-    				
-    				walllist.add(wa);
-    			}else if (line.contains("Shot")){
-    				//TODO muss noch implementiert werden
-    			}else if (line.contains("Pickup")){
-    				//TODO muss noch implementiert werden
-    			}
-    			
 
-        		
-        		Tank p = null;
-        		for (Tank tan : tanklist){
-        			if (tan.getName().equals("PlayerOne")){
-        				p = new Tank (tan.getName(), tan.getMaxlife(), tan.getLife(), tan.getMaxshot(), tan.getShot(), tan.getMaxmine(), tan.getMine(), tan.getStrength(), tan.getSpeed(), tan.getRotation(), tan.getScale(), tan.getX(), tan.getY());
-        				tanklist.remove(tan);
-        			}
-        		}
-        			
-        		Tank [] o = tanklist.toArray(new Tank [tanklist.size()]);
-        		Wall [] w = walllist.toArray(new Wall [walllist.size()]);
-        		Border [] b = borderlist.toArray(new Border [borderlist.size()]);
-        		
-        		
-        		
-        		gamelevel = new Level(m,b,p,o,w);
-    		}
-    	} catch (IOException e) {
-    		e.printStackTrace();
-    	}
-	}
+                } else if (line.contains("Tank")) {
+                    LinkedList<String> ll = new LinkedList<String>();
+                    for (int i = 0; i < line.length(); i++) {
+                        StringBuffer sb = new StringBuffer();
+                        while (i < line.length() && line.charAt(i) != ' ') {
+                            sb.append(line.charAt(i));
+                            i++;
+                        }
+                        ll.add(sb.toString().replaceAll("\"", ""));
+                    }
+                    Tank ta = new Tank(ll.get(1), Integer.valueOf(2), Integer.valueOf(3), Integer.valueOf(4), Integer.valueOf(5), Integer.valueOf(6), Integer.valueOf(7), Integer.valueOf(8), Integer.valueOf(9), Integer.valueOf(10), Integer.valueOf(11), Integer.valueOf(12), Integer.valueOf(13));
 
-	@Override
-	public void render(GameContainer container, StateBasedGame game, Graphics g)
-			throws SlickException {
-		entityManager.renderEntities(container, game, g);
-		
-	}
+                    tanklist.add(ta);
+                } else if (line.contains("Wall")) {
+                    LinkedList<String> ll = new LinkedList<String>();
+                    for (int i = 0; i < line.length(); i++) {
+                        StringBuffer sb = new StringBuffer();
+                        while (i < line.length() && line.charAt(i) != ' ') {
+                            sb.append(line.charAt(i));
+                            i++;
+                        }
+                        ll.add(sb.toString());
+                    }
+                    Wall wa = new Wall(Integer.valueOf(1), Integer.valueOf(2), Integer.valueOf(3),
+                            Integer.valueOf(4), Integer.valueOf(5), Integer.valueOf(6));
 
-	@Override
-	public void update(GameContainer container, StateBasedGame game, int delta)
-			throws SlickException {
-		entityManager.updateEntities(container, game, delta);
-		
-	}
+                    walllist.add(wa);
+                } else if (line.contains("Shot")) {
+                    //TODO muss noch implementiert werden
+                } else if (line.contains("Pickup")) {
+                    //TODO muss noch implementiert werden
+                }
 
-	@Override
-	public int getID() {
-		return stateID;
-	}
-	
+                Tank p = null;
+                // TODO: Error in implementation, has to be fixed
+//                for (Tank tan : tanklist) {
+//                    if (tan.getName().equals("PlayerOne")) {
+//                        p = new Tank(tan.getName(), tan.getMaxlife(), tan.getLife(), tan.getMaxshot(), tan.getShot(), tan.getMaxmine(), tan.getMine(), tan.getStrength(), tan.getSpeed(), tan.getRotation(), tan.getScale(), tan.getX(), tan.getY());
+//                        tanklist.remove(tan);
+//                    }
+//                }
+
+                Tank[] o = tanklist.toArray(new Tank[tanklist.size()]);
+                Wall[] w = walllist.toArray(new Wall[walllist.size()]);
+                Border[] b = borderlist.toArray(new Border[borderlist.size()]);
+
+
+
+                gamelevel = new Level(m, b, p, o, w);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void render(GameContainer container, StateBasedGame game, Graphics g)
+            throws SlickException {
+        entityManager.renderEntities(container, game, g);
+
+    }
+
+    @Override
+    public void update(GameContainer container, StateBasedGame game, int delta)
+            throws SlickException {
+        entityManager.updateEntities(container, game, delta);
+
+    }
+
+    @Override
+    public int getID() {
+        return stateID;
+    }
 }
--- SS12_GDI1_PJT_Tank/src/TankMain/MainMenu.java
+++ SS12_GDI1_PJT_Tank/src/TankMain/MainMenu.java
@@ -1,10 +1,11 @@
 package TankMain;
 
-import java.io.File;
-
-import javax.swing.JFileChooser;
-import javax.swing.filechooser.FileFilter;
-
+import eea.engine.action.Action;
+import eea.engine.action.basicactions.*;
+import eea.engine.component.render.*;
+import eea.engine.entity.*;
+import eea.engine.event.*;
+import eea.engine.event.basicevents.*;
 import org.newdawn.slick.Color;
 import org.newdawn.slick.GameContainer;
 import org.newdawn.slick.Graphics;
@@ -14,52 +15,42 @@ import org.newdawn.slick.geom.Vector2f;
 import org.newdawn.slick.state.BasicGameState;
 import org.newdawn.slick.state.StateBasedGame;
 
-import eea.engine.action.Action;
-import eea.engine.action.basicactions.*;
-import eea.engine.component.Component;
-import eea.engine.component.render.*;
-import eea.engine.entity.*;
-import eea.engine.event.*;
-import eea.engine.event.basicevents.*;
+public class MainMenu extends BasicGameState {
 
-
-
-public class MainMenu extends BasicGameState{
-	private int stateID; 							// Identifier von diesem BasicGameState
-	private StateBasedEntityManager entityManager; 	// zugehoeriger entityManager
-	
-	private final int distance = 40;
+    private int stateID; 							// Identifier von diesem BasicGameState
+    private StateBasedEntityManager entityManager; 	// zugehoeriger entityManager
+    private final int distance = 40;
     private final int start_Position = 240;
-    
+
     MainMenu(int id) {
-       stateID = id;
-       entityManager = StateBasedEntityManager.getInstance();
+        stateID = id;
+        entityManager = StateBasedEntityManager.getInstance();
     }
-    
+
     /**
      * Wird vor dem (erstmaligen) Starten dieses State's ausgefuehrt
      */
     @Override
-	public void init(GameContainer container, StateBasedGame game) throws SlickException {
-    	// Hintergrund
-    	Entity background = new Entity("menu");	
-    	background.setPosition(new Vector2f(400,300));
-    	background.addComponent(new ImageRenderComponent(new Image("/assets/menu.png")));    	    	
-    	entityManager.addEntity(stateID, background);
-    	
-    	// New Game
-    	String new_Game = "Neues Spiel starten";
-    	Entity new_Game_Entity = new Entity(new_Game);
-    	new_Game_Entity.setPosition(new Vector2f(700, 250));
-    	new_Game_Entity.setScale(0.15f);
-    	new_Game_Entity.addComponent(new ImageRenderComponent(new Image("assets/entry.png")));
-    	
-    	// Erstelle das Ausloese-Event und die zugehoerige Action
-    	ANDEvent mainEvents = new ANDEvent(new MouseEnteredEvent(), new MouseClickedEvent());
-    	Action new_Game_Action = new ChangeStateInitAction(Launch.GAME);
-  
-    	new_Game_Entity.addComponent(mainEvents);
-    	
+    public void init(GameContainer container, StateBasedGame game) throws SlickException {
+        // Hintergrund
+        Entity background = new Entity("menu");
+        background.setPosition(new Vector2f(400, 300));
+        background.addComponent(new ImageRenderComponent(new Image("/assets/menu.png")));
+        entityManager.addEntity(stateID, background);
+
+        // New Game
+        String new_Game = "Neues Spiel starten";
+        Entity new_Game_Entity = new Entity(new_Game);
+        new_Game_Entity.setPosition(new Vector2f(700, 250));
+        new_Game_Entity.setScale(0.15f);
+        new_Game_Entity.addComponent(new ImageRenderComponent(new Image("assets/entry.png")));
+
+        // Erstelle das Ausloese-Event und die zugehoerige Action
+        ANDEvent mainEvents = new ANDEvent(new MouseEnteredEvent(), new MouseClickedEvent());
+        Action new_Game_Action = new ChangeStateInitAction(Launch.GAME);
+
+        new_Game_Entity.addComponent(mainEvents);
+
 //    	Action loading = new Action(){
 //
 //			@Override
@@ -78,7 +69,7 @@ public class MainMenu extends BasicGameState{
 //					public boolean accept(File datei){
 //						return datei.isDirectory()|| datei.getName().toLowerCase().endsWith(t);
 //					}
-//					
+//
 //					public String getDescription(){
 //						if(t.equals(".txt"))
 //							return ".tanks";
@@ -93,61 +84,59 @@ public class MainMenu extends BasicGameState{
 //					return auswahl.getSelectedFile();
 //				else return null;
 //			}
-//    		
+//
 //    	};
-   	mainEvents.addAction(new_Game_Action);
-//    	
-    	
-    	
-    	// Fuege die Entity zum StateBasedEntityManager hinzu
-    	entityManager.addEntity(this.stateID, new_Game_Entity);
-    	
-    	// Beenden
-    	Entity quit_Entity = new Entity("Beenden");
-    	quit_Entity.setPosition(new Vector2f(700, 290));
-    	quit_Entity.setScale(0.15f);
-    	quit_Entity.addComponent(new ImageRenderComponent(new Image("assets/entry.png")));
-    	
-    	// Erstelle das Ausloese-Event und die zugehoerige Action
-    	ANDEvent mainEvents_q = new ANDEvent(new MouseEnteredEvent(), new MouseClickedEvent());
-    	Action quit_Action = new QuitAction();
-    	mainEvents_q.addAction(quit_Action);
-    	quit_Entity.addComponent(mainEvents_q);
-    	
-    	// Fuege die Entity zum StateBasedEntityManager hinzu
-    	entityManager.addEntity(this.stateID, quit_Entity);
-    	
-    }
-    
+        mainEvents.addAction(new_Game_Action);
+//
 
+
+        // Fuege die Entity zum StateBasedEntityManager hinzu
+        entityManager.addEntity(this.stateID, new_Game_Entity);
+
+        // Beenden
+        Entity quit_Entity = new Entity("Beenden");
+        quit_Entity.setPosition(new Vector2f(700, 290));
+        quit_Entity.setScale(0.15f);
+        quit_Entity.addComponent(new ImageRenderComponent(new Image("assets/entry.png")));
+
+        // Erstelle das Ausloese-Event und die zugehoerige Action
+        ANDEvent mainEvents_q = new ANDEvent(new MouseEnteredEvent(), new MouseClickedEvent());
+        Action quit_Action = new QuitAction();
+        mainEvents_q.addAction(quit_Action);
+        quit_Entity.addComponent(mainEvents_q);
+
+        // Fuege die Entity zum StateBasedEntityManager hinzu
+        entityManager.addEntity(this.stateID, quit_Entity);
+
+    }
 
     /**
      * Wird vor dem Frame ausgefuehrt
      */
     @Override
-	public void update(GameContainer container, StateBasedGame game, int delta)
-			throws SlickException {
-		entityManager.updateEntities(container, game, delta);
-	}
-    
+    public void update(GameContainer container, StateBasedGame game, int delta)
+            throws SlickException {
+        entityManager.updateEntities(container, game, delta);
+    }
+
     /**
      * Wird mit dem Frame ausgefuehrt
      */
-	@Override
-	public void render(GameContainer container, StateBasedGame game, 
-												Graphics g) throws SlickException {
-		entityManager.renderEntities(container, game, g);
-		
-		int counter = 0;
-		g.setColor(Color.black);
-		g.drawString("Neues Spiel", 620, start_Position+counter*distance); counter++;
-		g.drawString("Beenden", 620, start_Position+counter*distance); counter++;
-	}
+    @Override
+    public void render(GameContainer container, StateBasedGame game,
+            Graphics g) throws SlickException {
+        entityManager.renderEntities(container, game, g);
 
-	@Override
-	public int getID() {
-		return stateID;
-	}
-	
+        int counter = 0;
+        g.setColor(Color.black);
+        g.drawString("Neues Spiel", 620, start_Position + counter * distance);
+        counter++;
+        g.drawString("Beenden", 620, start_Position + counter * distance);
+        counter++;
+    }
 
+    @Override
+    public int getID() {
+        return stateID;
+    }
 }
