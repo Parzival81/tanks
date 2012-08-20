4cc305c7f36eda7cd108253fa5e24cc335e77676
jerry-hsu <xion_@hotmail.de>
2012-08-20 09:10:37 -0700

    AiBasic.java

--- src/de/tu_darmstadt/gdi1/tanks/components/AiBasic.java
+++ src/de/tu_darmstadt/gdi1/tanks/components/AiBasic.java
@@ -1,8 +1,21 @@
 package de.tu_darmstadt.gdi1.tanks.components;
 
+import java.util.Random;
+import java.util.Timer;
+
+import org.newdawn.slick.GameContainer;
+import org.newdawn.slick.Input;
+import org.newdawn.slick.state.StateBasedGame;
+
+import de.tu_darmstadt.gdi1.tanks.entity.Tank;
 import eea.engine.action.basicactions.MoveForwardAction;
 import eea.engine.entity.Entity;
+import eea.engine.event.ANDEvent;
+import eea.engine.event.Event;
 import eea.engine.event.basicevents.CollisionEvent;
+import eea.engine.event.basicevents.KeyDownEvent;
+import eea.engine.event.basicevents.LoopEvent;
+import eea.engine.event.basicevents.MovementDoesntCollideEvent;
 
 /**
  * Basic AI for the game
@@ -10,9 +23,51 @@ import eea.engine.event.basicevents.CollisionEvent;
  * @author Patrick
  */
 public class AiBasic {
-
-    public AiBasic(Entity tank) {
-
-        MoveForwardAction mfa = new MoveForwardAction(1f);
+	private int difficulty;
+	private Entity tank;
+	
+    public AiBasic(Entity tank, int difficulty) {
+    	this.tank = tank;
+    	this.difficulty = difficulty;
+    	
+    	switch (difficulty){
+    		case 1: move1();
+    		break;
+    		case 2: move2();
+    		break;
+    		case 3: move3();
+    		break;
+    	}
+    	
     }
+    
+    public void move1(){
+    	
+    		Random rg = new Random();
+    		
+    		int number  = rg.nextInt(3);
+    		
+
+			((Tank) tank).steerLeft(new ANDEvent
+					(new LoopEvent(),new CollisionEvent()));
+			
+    		((Tank) tank).steerForward(new ANDEvent
+    				(new LoopEvent(),new MovementDoesntCollideEvent(2, new MoveForwardAction(0.05f))));
+    		
+			
+//    		 ((Tank) tank).steerRight(new ANDEvent
+//    				 (new LoopEvent(),new MovementDoesntCollideEvent(2, new MoveForwardAction(0.05f))));
+		    
+    		
+    		
+    		
+    	
+    }
+    public void move2(){
+    	
+    }
+    public void move3(){
+    	
+    }
+    
 }
